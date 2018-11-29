b1975636c1a872d690d84cde0ea7ce93975aefea
wenpeng <yygy04eeq12>
2018-11-29 14:17:43 +0800

    前端

---
+++ WebServer/src/main/java/com/wp/hny/web/controller/IndexController.java
@@ -0,0 +1,24 @@
+package com.wp.hny.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 
+ * @author swiftwen
+ * @date 2018年11月23日 下午4:19:07
+ */
+@Controller
+public class IndexController {
+	
+	
+	@RequestMapping(value = "/homepage",method = RequestMethod.GET)
+    public String say(HttpServletRequest request, HttpServletResponse response)throws Exception {
+		
+		return "index";
+    }
+}
